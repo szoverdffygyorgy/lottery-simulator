f1fb94477e3c415f816d37de1d2b7f7e3f7dee9e
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2023-03-06 15:19:24 +0100

    Removed unused variable

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,14 +1,12 @@
 import { useCallback } from 'react';
 import useSaveProgress from '../../hooks/use-save-progress/use-save-progress';
 import useDispatchContext from '../../state/context/dispatch/use-dispatch-context';
-import useStateContext from '../../state/context/state/use-state-context';
 import MainContainer from '../main-container/main-container';
 import Title from '../title/title';
 import { ResetButton, Container } from './app.styles';
 
 function App() {
   const dispatch = useDispatchContext();
-  const state = useStateContext();
 
   useSaveProgress();
 
